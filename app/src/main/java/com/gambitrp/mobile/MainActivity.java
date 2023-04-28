9124557ac75467a6aa08a9cd83767999d66b2e78
Данил_Волк Данил_Волк <danil222223333@gmail.com>
2023-04-28 04:34:08 +0300

    struct packet

---
+++ app/src/main/java/com/gambitrp/mobile/MainActivity.java
@@ -0,0 +1,85 @@
+package com.gambitrp.mobile;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import com.gambitrp.mobile.function.WebSocket;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    private WebView client;
+    private WebSocket ws;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        client =(WebView) findViewById(R.id.webview);
+        client.setWebViewClient(new WebViewClient());
+        WebSettings webSettings=client.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        client.setWebViewClient(new WebClient());
+        client.setWebChromeClient(new ChromeClient());
+        client.loadUrl("file:///android_asset/index.html");
+        client.addJavascriptInterface(new JavaScriptInterface(this), "javafunc");
+        try {
+            ws = new WebSocket(new URI(
+                    "ws://localhost:4327/launcher"));
+            ws.connect();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public class WebClient extends WebViewClient{
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon){
+            super.onPageStarted(view,url,favicon);
+        }
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view,String url){
+            view.loadUrl(url);
+            return true;
+        }
+    }
+    @Override
+    public void onBackPressed(){
+        if(client.canGoBack()) {
+            client.goBack();
+        }
+        else{
+            super.onBackPressed();
+        }
+    }
+    public class ChromeClient extends WebChromeClient {
+        //Обработка событий JS
+    }
+
+    public class JavaScriptInterface {
+        Context mContext;
+
+        JavaScriptInterface(Context c) {
+            mContext = c;
+        }
+
+        @JavascriptInterface
+        public boolean testFunc(String test) {
+            System.out.println(test);
+            return true;
+        }
+    }
+}
